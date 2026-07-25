d4ff721bf98870c2befd8aaa1971b1b2f3f7dda2
anwer1919 <98690064+anwer1919@users.noreply.github.com>
2026-07-26 01:05:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const bcrypt = require('bcryptjs');
 const db = require('./database');
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 app.use(cors());
 app.use(express.json());
